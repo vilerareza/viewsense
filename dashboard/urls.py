d991de05f7ec766ac37a901ece588526eaf9959a
Reza Vilera <vilera_reza99@yahoo.com>
2021-07-07 23:33:37 +0700

    modify urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="dashboard_url"),
     #path('stream.mjpg', views.start_stream, name="stream"),
-    path('stream.mjpg', views.start_stream, name="stream"),
-    path('status', views.statusUpdate, name="statusUpdate")
+    #path('stream.mjpg', views.start_stream, name="stream"),
+    #path('status', views.statusUpdate, name="statusUpdate")
 ]

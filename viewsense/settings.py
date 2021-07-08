bae3f2c8388c6adeb140802b65f871b7a7c2a7b8
Reza Vilera <vilera_reza99@yahoo.com>
2021-07-08 22:10:32 +0700

    settings

--- viewsense/settings.py
+++ viewsense/settings.py
@@ -126,8 +126,11 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATIC_URL = STATIC_ROOT
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

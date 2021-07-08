d8c31535ad4a4f779dfecaf930905e15e1efdf39
Reza Vilera <vilera_reza99@yahoo.com>
2021-07-08 20:39:43 +0700

    modify setting for static URL

--- viewsense/settings.py
+++ viewsense/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = '/static/'
+
+STATIC_URL = STATIC_ROOT
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
